58ee4516fa955087a421465845cc652c4c38454c
Silahanim21 <turkcell63632@gmail.com>
2024-10-10 20:10:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -15,9 +15,10 @@ class Config(object):
     SPAMWATCH_API = "t9HHtrsmy7faPQWloX8xCvdZK~puDP2RnHLpb~qijQqDj94mhcMQdDP_xO0a_Iwe"
     TOKEN = getenv("TOKEN")
     OWNER_ID = int(getenv("OWNER_ID", 5938660179))
-    OWNER_USERNAME = getenv("OWNER_USERNAME", "JARVIS_V2")
-    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "Dora_Hub")
-    LOGGER_ID = int(getenv("LOGGER_ID", "-1002014167331"))
+    OWNER_USERNAME = getenv("OWNER_USERNAME", "sevimsiz_biri")
+    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "Gece
+Sohbettr")
+    LOGGER_ID = int(getenv("LOGGER_ID", "-1002065943011"))
     MONGO_URI = getenv("MONGO_DB_URI")
     DB_NAME = getenv("DB_NAME", "AvaRobot")
     REDIS_URL = "redis://default:wK6ZCiclq4iQKYpgfY90v6kd6WdPfEwl@redis-10186.c263.us-east-1-2.ec2.cloud.redislabs.com:10186/default"
